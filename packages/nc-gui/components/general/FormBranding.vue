0b747b21fec047becfd3bd038f469e93b932c0cb
Elijah Williams <Elijah@nupixl.com>
2026-01-18 20:44:30 -0500

    Update form branding to Nupixl

--- packages/nc-gui/components/general/FormBranding.vue
+++ packages/nc-gui/components/general/FormBranding.vue
@@ -1,12 +1,19 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { $e } = useNuxtApp()
+
+const openLink = (url: string) => {
+  $e('c:branding:click')
+  window.open(url, '_blank', 'noopener,noreferrer')
+}
+</script>
 
 <template>
   <div
     class="flex items-center gap-3 cursor-pointer text-gray-700 dark:text-slate-300"
-    @click="openLink('https://www.nocodb.com')"
+    @click="openLink('https://nupixl.com')"
   >
-    <GeneralIcon icon="nocodb1" class="w-7 h-7 -my-0.5" />
+    <img src="/img/brand/nupixl-color.png" class="w-7 h-7 -my-0.5" alt="Nupixl" />
 
-    <div class="text-sm">NocoDB Forms</div>
+    <div class="text-sm">Nupixl Forms</div>
   </div>
 </template>
