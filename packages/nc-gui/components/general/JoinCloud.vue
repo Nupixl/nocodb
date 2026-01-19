bd5777e20c83545cbf82b575427912ae99049837
Elijah Williams <Elijah@nupixl.com>
2026-01-18 21:00:35 -0500

    Update cloud CTA to Nupixl

--- packages/nc-gui/components/general/JoinCloud.vue
+++ packages/nc-gui/components/general/JoinCloud.vue
@@ -73,11 +73,11 @@ const onMouseover = async () => {
     class="flex flex-row items-center w-full bg-bg-nc-bg-default rounded-lg border-1 border-nc-border-brand shadow-sm mb-0.5 overflow-hidden"
   >
     <a
-      v-e="['c:navbar:join-cloud']"
+      v-e="['c:branding:join-cloud']"
       class="flex flex-grow !no-underline items-center justify-center border-r-1 h-full hover:bg-nc-bg-gray-light"
-      href="https://app.nocodb.com/#/signin?utm_source=OSS&utm_medium=OSS&utm_campaign=OSS&utm_content=OSS"
+      href="https://app.nupixl.com/#/signin?utm_source=OSS&utm_medium=OSS&utm_campaign=OSS&utm_content=OSS"
     >
-      <div class="px-1 text-nc-content-gray-muted prose-sm" style="line-height: 1.3125rem">Try NocoDB Cloud</div>
+      <div class="px-1 text-nc-content-gray-muted prose-sm" style="line-height: 1.3125rem">Try Nupixl Cloud</div>
     </a>
 
     <a-tooltip arrow-point-at-center overlay-class-name="nc-join-cloud-tooltip">
@@ -88,7 +88,7 @@ const onMouseover = async () => {
         <div class="w-70.5 bg-transparent overflow-hidden rounded-2xl shadow border-1 border-nc-border-gray-medium">
           <div class="p-4 bg-nc-bg-default gap-4 inline-flex flex-col w-full">
             <div class="flex items-center gap-3">
-              <div class="text-base text-nc-content-gray-emphasis font-bold flex-1">NocoDB Cloud</div>
+              <div class="text-base text-nc-content-gray-emphasis font-bold flex-1">Nupixl Cloud</div>
               <div class="text-caption px-1 rounded-md bg-nc-bg-brand text-nc-content-brand">Usage based</div>
             </div>
 
@@ -125,7 +125,7 @@ const onMouseover = async () => {
               <div class="text-xs leading-[18px] font-normal text-nc-content-gray-muted text-center">
                 (no credit card required)
               </div>
-              <a href="https://app.nocodb.com/#/signin" target="_blank" class="!no-underline" rel="noopener">
+              <a href="https://app.nupixl.com/#/signin" target="_blank" class="!no-underline" rel="noopener">
                 <NcButton type="secondary" class="w-full">Start for Free</NcButton>
               </a>
             </div>
